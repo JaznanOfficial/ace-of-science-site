256161976987ac86bf9a6c3f4c8525329029e482
JaznanOfficial <jaznanofficial@gmail.com>
2022-01-30 17:54:26 +0600

    merge login to main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
+import Login from './Components/LogIn/Login';
 
 function App() {
   return (
     <div className="App">
-    <Navigation></Navigation>
+      <Navigation></Navigation>
+      <Login></Login>
     </div>
   );
 }
--- src/Components/LogIn/Login.js
+++ src/Components/LogIn/Login.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import './Login.css'
 
 const Login = () => {
     return (
-        <div>
-            
+
+       
+        <div class="form-area">
+                           
+             <div class="form-text-side">
+                <h1><i class="fas fa-rocket animate-bounce"></i></h1> <br/>
+                <h2> ACE of science </h2>
+                <h3 >Feel free to learn,spread what you have learned </h3>
+                <button class="shadow-2xl">  আপনার অ্যাকাউন্ট নেই ? অ্যাকাউন্ট তৈরি করুন<i class="fas fa-fighter-jet"></i></button>
+             </div>  
+             
+           
+             
+         
+             <div class="form-input-side">
+                <form action="post">
+                          <h1><l class="fas fa-globe-asia animate-spin  animate-bounce"></l></h1>
+                          <i class="far fa-user"></i> <input type="text"  placeholder="আপনার নাম লিখুন "/> <br/>
+                          <i class="far fa-envelope"></i> <input type="email" placeholder="আপনার ইমেইল লিখুন"/> <br/>
+                          <i class="fas fa-key"></i> <input type="password" placeholder="আপনার পাসওয়ার্ড লিখুন"/><br/>
+       
+                <button class="shadow-2xl">লগইন</button> <br/>
+              </form>
+                
+             </div>   
         </div>
+
+
     );
 };
 
