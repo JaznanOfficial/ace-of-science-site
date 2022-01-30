f904ec40dc8f6ecaec2374f45650e7ec38571a35
JaznanOfficial <jaznanofficial@gmail.com>
2022-01-30 18:11:37 +0600

    sign up done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Login from './Components/LogIn/Login';
+import Signup from './Components/Signup/Signup';
 
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
       <Login></Login>
+      <Signup></Signup>
     </div>
   );
 }
