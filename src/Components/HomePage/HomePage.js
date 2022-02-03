f1c94f86deb9e681e6c873343517db8b870e447f
JaznanOfficial <jaznanofficial@gmail.com>
2022-02-03 13:00:01 +0600

    messeging added

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import HeaderBanner from '../Header/HeaderBanner';
-import Category from '../Category/Category';
-import MagazineSlider from '../MagazineSlider/MagazineSlider';
-import Achievement from '../Achievement/Achievement';
-import Testimonials from '../Testimonials/Testimonials'
+import React from "react";
+import HeaderBanner from "../Header/HeaderBanner";
+import Category from "../Category/Category";
+import MagazineSlider from "../MagazineSlider/MagazineSlider";
+import Achievement from "../Achievement/Achievement";
+import Testimonials from "../Testimonials/Testimonials";
 import ScrollToTop from "react-scroll-to-top";
-
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const HomePage = () => {
     return (
         <div>
-        <ScrollToTop smooth color='#FE1A00' viewBox="0 0 250 250" />
-        <HeaderBanner></HeaderBanner>
-        <Category></Category>
-        <MagazineSlider></MagazineSlider>
-        <Achievement></Achievement>
-        <Testimonials></Testimonials>
+            <ScrollToTop smooth color="#FE1A00" viewBox="0 0 250 250" />
+            <MessengerCustomerChat pageId="103023455276751" appId="1147330332685692" htmlRef="<REF_STRING>" />
+            <HeaderBanner></HeaderBanner>
+            <Category></Category>
+            <MagazineSlider></MagazineSlider>
+            <Achievement></Achievement>
+            <Testimonials></Testimonials>
         </div>
     );
 };
