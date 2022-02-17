440a22bd65b9d735579f2ee88f6c926c3d15dd97
JaznanOfficial <jaznanofficial@gmail.com>
2022-02-17 07:46:14 +0600

    some changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,37 +1,32 @@
 import React from "react";
 import "./Footer.css";
 import logo from "./ace-of-science.png";
-import Typical from 'react-typical'
+import Typical from "react-typical";
 
 const Footer = () => {
     return (
-        
-            <div className="footer">
-                <div className="d-lg-flex flex-row justify-content-center align-items-center py-5 px-3">
+        <div className="footer">
+            <div className="d-lg-flex flex-row justify-content-center align-items-center py-5 px-3">
+                <div className="footer-left text-start">
+                    <img src={logo} alt="img not found" width={300} />
                     <div>
-                        <img src={logo} alt="img not found" width={300} />
-                    </div>
-                    <div className="ms-3 ps-3 footer-right">
-                        <strong>
-                            <h4 className="text-start">
-                                AOS is a online guideline platform for secondary and higher
-                                secondary students.We help students to learn their subjects easily,
-                                provide online classes, taking online exams ,giving hand notes and
-                                so on.
-                            </h4>
+                        © All rights reserved by{" "}
+                        <strong className="ms-1">
+                            <Typical className='d-inline' steps={["ACE OF SCIENCE", 1000, "", 500]} loop={Infinity} />
                         </strong>
                     </div>
                 </div>
-                <div className="footer-bottom mx-auto d-flex justify-content-center">
-                © All rights reserved by  <strong className="ms-1"><Typical
-                steps={['ACE OF SCIENCE', 1000, '', 500]}
-                loop={Infinity}
-                
-              />
-                </strong>
+                <div className="ms-3 ps-3 footer-right">
+                    <strong>
+                        <h4 className="text-start">
+                            AOS is a online guideline platform for secondary and higher secondary
+                            students.We help students to learn their subjects easily, provide online
+                            classes, taking online exams ,giving hand notes and so on.
+                        </h4>
+                    </strong>
                 </div>
             </div>
-        
+        </div>
     );
 };
 
