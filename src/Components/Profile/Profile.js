8252dc239281ec07a3e8bf4842a2cbefaf01e5d4
JaznanOfficial <jaznanofficial@gmail.com>
2022-02-23 11:30:03 +0600

    some changes

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -24,7 +24,7 @@ const Profile = () => {
         });
     }
     return (
-        <div>
+        <div className="profile">
             <div class="page-content page-container" id="page-content">
                 <div class="padding">
                     <div class="row container d-flex justify-content-center">
