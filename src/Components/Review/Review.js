b0ebfb2e76c63f4e233ed1cfaf4205dbe020cac2
JaznanOfficial <jaznanofficial@gmail.com>
2022-03-28 15:05:33 +0600

    review post to database

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,10 +1,40 @@
 import React, { useRef } from "react";
 import Rating from "react-rating";
+import Swal from "sweetalert2";
 import "./Review.css";
 import review from "./review.svg";
 
 const Review = () => {
-    const form = useRef();
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const textRef = useRef();
+    const ratingRef = useRef();
+
+    const reviewHandler = (e) => {
+        e.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const text = textRef.current.value;
+        var rating = ratingRef.current.state.value;
+        const review = { name, email, text, rating };
+        console.log(ratingRef.current);
+
+        fetch("http://localhost:5000/review", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(review),
+        });
+        new Swal({
+            title: "Good job!",
+            text: "Your review successfully sent! Please stay with us",
+            icon: "success",
+        });
+
+        e.target.reset();
+     
+    };
     return (
         <div className="review">
             <div className="container d-lg-flex flex-row justify-content-between align-items-center my-5">
@@ -14,13 +44,25 @@ const Review = () => {
                 <div>
                     <div className="column right w-100 my-5">
                         <h1 className="text">Review us</h1>
-                        <form action="#" ref={form}>
+                        <form action="#" onSubmit={reviewHandler}>
                             <div className="fields">
                                 <div className="field name">
-                                    <input type="text" placeholder="Name" name="name" required />
+                                    <input
+                                        type="text"
+                                        placeholder="Name"
+                                        name="name"
+                                        ref={nameRef}
+                                        required
+                                    />
                                 </div>
                                 <div className="field email">
-                                    <input type="email" placeholder="Email" name="email" required />
+                                    <input
+                                        type="email"
+                                        placeholder="Email"
+                                        name="email"
+                                        ref={emailRef}
+                                        required
+                                    />
                                 </div>
                             </div>
 
@@ -30,11 +72,13 @@ const Review = () => {
                                     rows="10"
                                     placeholder="Review"
                                     name="review"
+                                    ref={textRef}
                                     required
                                 ></textarea>
                             </div>
                             <div className="field rating text-start">
                                 <Rating
+                                    ref={ratingRef}
                                     stop={5}
                                     emptySymbol={[
                                         "fa fa-star-o fa-2x low",
