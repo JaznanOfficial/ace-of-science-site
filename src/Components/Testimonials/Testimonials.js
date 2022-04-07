bf171fdd1f2c731909963e3158e2e49badd94f0c
JaznanOfficial <jaznanofficial@gmail.com>
2022-04-07 10:54:22 +0600

    some changes

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -24,7 +24,7 @@ const Testimonials = () => {
                             <Card>
                                 <Card.Body className="d-flex flex-column justify-content-center align-items-center">
                                     <div>
-                                    <Card.Text>{rating.text}</Card.Text>
+                                    <Card.Text>"{rating.text.slice(0,250)}..."</Card.Text>
                                     </div>
                                     </Card.Body>
                                     <div className="mb-2 mx-lg-5 mx-3 text-center">
