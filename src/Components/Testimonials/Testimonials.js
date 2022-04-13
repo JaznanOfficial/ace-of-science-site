18d92b072932cacf53bae5bf9fcefd7d4e98ef89
JaznanOfficial <jaznanofficial@gmail.com>
2022-04-13 18:09:50 +0600

    swiper adding

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import Rating from "react-rating";
+import { Swiper, SwiperSlide } from "swiper/react";
 import "./Testimonial.css";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Testimonials = () => {
     const [ratings, setRatings] = useState([]);
@@ -18,36 +23,51 @@ const Testimonials = () => {
             <section className="home-testimonial p-5">
                 <h2 className="pt-5">Explore the students experience</h2>
 
-                <Row xs={1} md={3} className="g-4 mt-5">
-                    {ratings.slice(0, 3).map((rating) => (
-                        <Col>
-                            <Card>
-                                <Card.Body className="d-flex flex-column justify-content-center align-items-center">
-                                    <div>
-                                    <Card.Text>"{rating.text.slice(0,250)}..."</Card.Text>
-                                    </div>
-                                    </Card.Body>
-                                    <div className="mb-2 mx-lg-5 mx-3 text-center">
-                                        <Rating
-                                            className="text-danger"
-                                            emptySymbol="fa fa-star-o"
-                                            fullSymbol="fa fa-star"
-                                            initialRating={rating.rating}
-                                            readonly
-                                        />
-                                    </div>
-                                <img
-                                    variant="top"
-                                    src={rating.photo}
-                                    alt="img not found"
-                                    className="mx-auto"
+
+
+                <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                >
+                
+                        {ratings.slice(0, 3).map((rating) => (
+                  
+                <SwiperSlide className="card">
+                    
+                        <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                            <div>
+                            <Card.Text>"{rating.text.slice(0,250)}..."</Card.Text>
+                            </div>
+                            </Card.Body>
+                            <div className="mb-2 mx-lg-5 mx-3 text-center">
+                                <Rating
+                                    className="text-danger"
+                                    emptySymbol="fa fa-star-o"
+                                    fullSymbol="fa fa-star"
+                                    initialRating={rating.rating}
+                                    readonly
                                 />
-                                <Card.Title>{rating.name}</Card.Title>
-                                <p>{rating.profession}</p>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
+                            </div>
+                        <img
+                            variant="top"
+                            src={rating.photo}
+                            alt="img not found"
+                            className="mx-auto"
+                        />
+                        <Card.Title>{rating.name}</Card.Title>
+                        <p>{rating.profession}</p>
+            
+                                </SwiperSlide>
+
+            ))}
+                
+ 
+              </Swiper>
+
+                
+                    
             </section>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "swiper/css/bundle";
 
 ReactDOM.render(
   <React.StrictMode>
