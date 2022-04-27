eff5b094d5893fbd98e8b67497b72bcc81c55527
JaznanOfficial <jaznanofficial@gmail.com>
2022-04-27 17:53:26 +0600

    notes complete

--- src/Components/SingleNotes/SingleNotes.js
+++ src/Components/SingleNotes/SingleNotes.js
@@ -1,28 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, Col, Row } from "react-bootstrap";
 import './SingleNotes.css'
 
 const SingleNotes = () => {
+
+    const [getNotes, setGetNotes] = useState([]);
+    useEffect(() => {
+        fetch("https://warm-citadel-00877.herokuapp.com/notes")
+            .then((res) => res.json())
+            .then((data) => {
+                setGetNotes(data);
+            });
+    }, []);
+
+    // console.log(getBlogs);
+    const reverseNotes = [...getNotes].reverse();
     return (
         <div className="single-note">
             <Row xs={1} md={4} className="g-2">
-                {Array.from({ length: 4 }).map((_, idx) => (
-                    <Col>
-                        <a href="">
-                        <Card className="overflow-hidden p-1">
+                {
+                    reverseNotes.map(notes => (
+                        <Col
+                    key={notes._id}
+                    >
+                        <a href={notes.pdfLink}>
+                        <Card className="overflow-hidden p-1 shadow-lg">
                         <Card.Body className="d-flex flex-row justify-content-center align-items-center text-decoration-none p-0">
                         <h2>
-                            <i class="fa-solid fa-file-pen mx-3"></i>
+                            <i class="fa-solid fa-file-pen mx-3 text-danger"></i>
                         </h2>
                             <p className="me-1">
-                            This is a longer card with supporting text below as a natural
+                            {notes.text}
                             
                             </p>
                             </Card.Body>
                         </Card>
                         </a>
-                    </Col>
-                ))}
+                    </Col>))
+                }
+                    
+        
             </Row>
         </div>
     );
