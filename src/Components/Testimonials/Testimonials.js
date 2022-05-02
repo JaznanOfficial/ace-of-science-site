92999025692c285ea673b11642a885e94a3f470d
JaznanOfficial <jaznanofficial@gmail.com>
2022-05-02 14:48:44 +0600

    logo changed

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -3,10 +3,10 @@ import { Card, Col, Row } from "react-bootstrap";
 import Rating from "react-rating";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./Testimonial.css";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 import { Pagination } from "swiper";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -19,51 +19,46 @@ const Testimonials = () => {
             .then((res) => res.json())
             .then((data) => setRatings(data));
     }, []);
-   
-  
+
     useEffect(() => {
-      AOS.init();
-      AOS.refresh();
+        AOS.init();
+        AOS.refresh();
     }, []);
 
     return (
-        <div className="mt-5" data-aos="zoom-in-up">
+        <div className="mt-5">
             <section className="home-testimonial p-5">
                 <h2 className="pt-5 mb-3">Explore the students experience</h2>
 
-
-
-                <Swiper
-                breakpoints={{
-                    // when window width is >= 640px
-                    640: {
-                      width: 640,
-                      slidesPerView: 1,
-                    },
-                    // when window width is >= 768px
-                    768: {
-                      width: 768,
-                      slidesPerView: 2,
-                    },
-                  }}
-                pagination={{
-                    dynamicBullets: true,
-                  }}
-                  modules={[Pagination]}
-                spaceBetween={50}
-                // slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
+          <Swiper
+          data-aos="zoom-in-up"
+                    breakpoints={{
+                        // when window width is >= 640px
+                        640: {
+                            width: 640,
+                            slidesPerView: 1,
+                        },
+                        // when window width is >= 768px
+                        768: {
+                            width: 768,
+                            slidesPerView: 2,
+                        },
+                    }}
+                    pagination={{
+                        dynamicBullets: true,
+                    }}
+                    modules={[Pagination]}
+                    spaceBetween={50}
+                    // slidesPerView={3}
+                    onSlideChange={() => console.log("slide change")}
+                    onSwiper={(swiper) => console.log(swiper)}
                 >
-                
-                        {ratings.slice(0, 9).map((rating) => (
-                  
-                <SwiperSlide className="card">
-                    
-                        <Card.Body className="d-flex flex-column justify-content-center align-items-center">
-                            <div>
-                            <Card.Text>"{rating.text.slice(0,250)}..."</Card.Text>
-                            </div>
+                    {ratings.slice(0, 9).map((rating) => (
+                        <SwiperSlide className="card">
+                            <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                                <div>
+                                    <Card.Text>"{rating.text.slice(0, 250)}..."</Card.Text>
+                                </div>
                             </Card.Body>
                             <div className="mb-2 mx-lg-5 mx-3 text-center">
                                 <Rating
@@ -74,24 +69,17 @@ const Testimonials = () => {
                                     readonly
                                 />
                             </div>
-                        <img
-                            variant="top"
-                            src={rating.photo}
-                            alt="img not found"
-                            className="mx-auto"
-                        />
-                        <Card.Title>{rating.name}</Card.Title>
-                        <p>{rating.profession}</p>
-            
-                                </SwiperSlide>
-
-            ))}
-                
- 
-              </Swiper>
-
-                
-                    
+                            <img
+                                variant="top"
+                                src={rating.photo}
+                                alt="img not found"
+                                className="mx-auto"
+                            />
+                            <Card.Title>{rating.name}</Card.Title>
+                            <p>{rating.profession}</p>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </section>
         </div>
     );
