97861009a0a14561ce65fab0e3a7de88ddaa22ba
JaznanOfficial <jaznanofficial@gmail.com>
2023-01-10 17:23:35 +0600

    more update

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,10 +1,16 @@
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    // apiKey: process.env.REACT_APP_API_KEY,
+    // authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    // projectId: process.env.REACT_APP_PROJECT_ID,
+    // storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    // messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    // appId: process.env.REACT_APP_APP_ID,
+    apiKey: "AIzaSyD3466wZ2-KEryLd-RKJL2nZj-LtKscwao",
+    authDomain: "ace-of-science.firebaseapp.com",
+    projectId: "ace-of-science",
+    storageBucket: "ace-of-science.appspot.com",
+    messagingSenderId: "673129090859",
+    appId: "1:673129090859:web:614734215b3268687c3f6e",
 };
-  
+
 export default firebaseConfig;
